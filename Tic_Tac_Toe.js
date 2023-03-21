0eb19e5dee2450673b46f964cbe14b8743b3b4f2
unknown <you@example.com>
2023-03-21 18:57:57 +0100

    add draw function and new Struction html

--- Tic_Tac_Toe.js
+++ Tic_Tac_Toe.js
@@ -6,11 +6,22 @@ let currentshape = 'cross';
 function fillshape(id) {
 
     if (currentshape == 'cross') {
-        currentshape = 'circel';
+        currentshape = 'circle';
     } else {
         currentshape = 'cross';
     }
     fields[id] = currentshape;
     console.log(fields);
+    draw();
 }
 
+function draw(){
+ for (let i = 0; i < fields.length; i++) {
+     if(fields[i] == 'circle')
+     { document.getElementById('Circle-' + i).classList.remove('d-none');
+    }
+    if(fields[i] == 'cross')
+    { document.getElementById('Cross-' + i).classList.remove('d-none');
+   }
+ }
+}
